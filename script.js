cf92ac4e83ea5a24cd081e28d5c4ba54b15c6b5a
Aniket Kaimal <aniket1995@yahoo.co.in>
2021-08-14 10:38:35 +0530

    1. Created a cell. 2. Applied the bgChange function on this cell. 3. Next action- Apply the bgChnage function on all the cells.

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-const container =document.getElementById("grid-container");
+const container = document.getElementById("grid-container");
 
-function makeRows(rows,cols){
+let cell = document.createElement("div");
+
+container.appendChild(cell).className = "grid-item";
+
+function random(number) {
+    return Math.floor(Math.random() * number + 1);
+}
+
+function makeRows(rows, cols) {
     container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-cols",cols);
-    for (c=0;c<(rows*cols);c++){
-        let cell=document.createElement("div");
-        // cell.innerText=(c+1);
-        container.appendChild(cell).className="grid-item";
+    container.style.setProperty("--grid-cols", cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
     };
 };
-makeRows(16,16);
+makeRows(16, 16);
+
+
+// let cell = document.createElement("div");
+// container.appendChild(cell).className = "grid-item";
+
+function bgChange() {
+    const rndCol = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+    cell.style.backgroundColor = rndCol;
+};
+
+cell.onmouseover = bgChange;
+
+
+
+
+
+
+
+
