5e3e078f0287bad60de0803358b627869bc33357
Aniket Kaimal <aniket1995@yahoo.co.in>
2021-08-15 18:59:21 +0530

    Added Hover effect on all cells.

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const container = document.getElementById("grid-container");
 
-let cell = document.createElement("div");
-
-container.appendChild(cell).className = "grid-item";
-
 function random(number) {
     return Math.floor(Math.random() * number + 1);
 }
@@ -13,23 +9,15 @@ function makeRows(rows, cols) {
     container.style.setProperty("--grid-cols", cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
+        // cell.innerText = (c + 1);
         container.appendChild(cell).className = "grid-item";
+        cell.addEventListener("mouseover", function bgChange(){  
+            const rndCol = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+            cell.style.backgroundColor = rndCol;
+        });
     };
 };
 makeRows(16, 16);
-
-
-// let cell = document.createElement("div");
-// container.appendChild(cell).className = "grid-item";
-
-function bgChange() {
-    const rndCol = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
-    cell.style.backgroundColor = rndCol;
-};
-
-cell.onmouseover = bgChange;
-
 
 
 
