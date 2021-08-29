187c0806a6c30ebc5620e12b270ccd3ac51ac139
Aniket Kaimal <aniket1995@yahoo.co.in>
2021-08-29 12:06:46 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function makeRows(rows, cols) {
     container.style.setProperty("--grid-cols", cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        // cell.innerText = (c + 1);
         container.appendChild(cell).className = "grid-item";
         cell.addEventListener("mouseover", function bgChange() {
             const rndCol = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
