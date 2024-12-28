02ce9f38a8ea55495f865fd715872cb3c553b2a3
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2024-12-28 12:44:25 +0530

    Add hover effect

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const container = document.getElementById('container');
-
 for(let i=0; i<256; i++){
     const square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
 }
+
+const squares = document.querySelectorAll('.square');
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = '#4CAF50';
+    });
+});
