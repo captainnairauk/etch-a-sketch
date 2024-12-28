89e46768694887c4871f7ba4950c798f0c9ca267
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2024-12-28 13:07:34 +0530

    Add createGrid() function

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const container = document.getElementById('container');
-for(let i=0; i<256; i++){
-    const square = document.createElement('div');
-    square.classList.add('square');
+const container = document.getElementById("container");
+const resizeButton = document.getElementById("resize-button");
+
+function createGrid(size) {
+  container.innerHTML = "";
+  const squareSize = 640 / size;
+
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
     container.appendChild(square);
+  }
+
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "#4CAF50";
+    });
+  });
+
 }
 
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = '#4CAF50';
-    });
-});
+
+createGrid(16);
+
+
