63397f2afac20e45af544a67d7ed9248e6b00ad8
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2024-12-28 13:29:48 +0530

    Add random color change

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const container = document.getElementById("container");
 const resizeButton = document.getElementById("resize-button");
 
+function getRandomRGB(){
+    const r = Math.floor(Math.random()*256);
+    const g = Math.floor(Math.random()*256);
+    const b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 function createGrid(size) {
   container.innerHTML = "";
   const squareSize = 640 / size;
@@ -10,13 +17,20 @@ function createGrid(size) {
     square.classList.add("square");
     square.style.width = `${squareSize}px`;
     square.style.height = `${squareSize}px`;
+    square.style.opacity = '0.1';
     container.appendChild(square);
   }
 
   const squares = document.querySelectorAll(".square");
   squares.forEach((square) => {
     square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "#4CAF50";
+      const randomColor = getRandomRGB();  
+      square.style.backgroundColor = randomColor;
+
+      let currentOpacity = parseFloat(square.style.opacity);
+      if(currentOpacity < 1){
+        square.style.opacity = (currentOpacity + 0.1).toString();
+      }
     });
   });
 
